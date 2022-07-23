6e2732e3d15209ee4ccf3f8073f8ae97d0da4f5c
rizalnf6 <rizalfarhannn@gmail.com>
2022-07-24 03:36:55 +0800

    repair logout module

--- routes/web.php
+++ routes/web.php
@@ -21,13 +21,17 @@ use Laravel\Fortify\Http\Responses\LogoutResponse;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('tes', function() {
+    return "blok";
+});
+
 Route::group(['middleware' => ['auth:sanctum', 'verified']], function () {
    
     Route::name('dashboard.')->prefix('dashboard')->group(function () {
         Route::get('/', [DashboardController::class, 'index'])->name('index');
 
         Route::middleware(['admin'])->group(function () {
-            Route::resource('product', ProductController::class);
+            // Route::resource('product', ProductController::class);
             Route::resource('category', ProductCategoryController::class);
             Route::resource('product.gallery', ProductGalleryController::class)->shallow()->only([
                 'index', 'create', 'store', 'destroy'
